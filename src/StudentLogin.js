48911721c2600019cdb632c44c41cc0f920589d5
Daniel Adea <danielnadea@gmail.com>
2019-02-17 13:01:17 -0800

    fixed student login parameters

--- src/StudentLogin.js
+++ src/StudentLogin.js
@@ -6,8 +6,6 @@ render() {
         <form>
         <p>Username:</p>
         <input type="JRob" placeholder="test" required/>
-        <p>Email:</p>
-        <input type = "text" placeholder = "JackRob42@ucla.edu" required/> 
         <p>UID</p>
         <input type = "text" placeholder = "424242424" required/> 
         <p>Password:</p>
