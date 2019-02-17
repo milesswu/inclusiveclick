fd8c4ae2bc0a63923a25b6d96324ab1999ba90d1
Daniel Adea <danielnadea@gmail.com>
2019-02-17 13:30:25 -0800

    update student course popup

--- src/StudentCourses.js
+++ src/StudentCourses.js
@@ -1,12 +1,16 @@
 import React from 'react';
 class StudentCourses extends React.Component {
+    
 render() {
       return (
           <div>
          <ul>
              <li>CS 32</li>
          </ul>
-                  <button > Add Courses</button>
+  <Popup trigger={<button> Add Courses</button>} position="right center">
+    <div>Code</div>
+  </Popup>
+);
           </div>
       );
     }
