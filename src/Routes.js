ea0edbeb058c37d5f23a461446a5e2cef5e75181
milesswu <mileswu13@gmail.com>
2019-02-17 17:16:54 -0800

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import ProfLogin from './Pages/ProfLogin';
 import StudentLogin from './Pages/StudentLogin';
 import StudentRegister from './Pages/StudentRegister';
 import StudentCourses from './Pages/StudentCourses';
+import ProfCreatePoll from './Pages/ProfCreatePoll';
 
 class Routes extends React.Component {
     render() {
@@ -15,6 +16,7 @@ class Routes extends React.Component {
                 <Route path='/student-login' component={StudentLogin}/>
                 <Route path='/student-register' component={StudentRegister}/>
                 <Route path='/courses' component={StudentCourses}/>
+                <Route path='/prof-poll-create' component={ProfCreatePoll}/>
             </Switch>
         );
     }
